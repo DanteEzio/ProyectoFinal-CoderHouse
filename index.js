d15b791348d160f3184ed4a57c6744828c72242d
DanteEzio <quantum120@hotmail.com>
2022-05-06 18:59:08 -0500

    Se modifico el diseño de la navbar

--- index.js
+++ index.js
@@ -23,3 +23,35 @@ $(".bubble-item").mouseleave(function () {
 });
 
 // ******************************************
+
+$("li").hover(
+  function () {
+    $(this)
+      .find("span")
+      .stop()
+      .animate(
+        {
+          width: "100%",
+          opacity: "1",
+        },
+        400,
+        function () {}
+      );
+  },
+  function () {
+    $(this)
+      .find("span")
+      .stop()
+      .animate(
+        {
+          width: "0%",
+          opacity: "0",
+        },
+        400,
+        function () {}
+      );
+  }
+);
+
+///////////////////////////////////////
+
